b75ff4f64490083773813577b1ac5a3baa766206
Tony Xiao <loading800@gmail.com>
2026-01-11 18:49:02 -0800

    Fix syntax error in AdminPanel hide button

--- src/components/AdminPanel.jsx
+++ src/components/AdminPanel.jsx
@@ -1114,7 +1114,6 @@ export default function AdminPanel({
                                                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z" />
                                                 </svg>
                                             </button>
-                                            </button>
                                             <button
                                                 onClick={() => onUpdateItem(item.id, { hidden: !item.hidden })}
                                                 className={`p-2 hover:bg-gray-100 dark:hover:bg-gray-800 rounded-lg transition-colors ${item.hidden ? 'text-gray-400 hover:text-primary-500' : 'text-primary-500 hover:text-gray-400'}`}
